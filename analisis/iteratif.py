9d2fccc40622eae6df2e6cc0ed75868f3256d95e
BrianAlfredoAP <brianalfredodo@gmail.com>
2026-01-03 14:50:11 +0700

    Menambahkan fungsi pantulan_iteratif dan simulasi di main

--- analisis/iteratif.py
+++ analisis/iteratif.py
@@ -1,6 +1,21 @@
 def pantulan_iteratif(h0, e, h_min):
-    """
-    Fungsi simulasi pantulan bola secara iteratif
-    """
-    # inisialisasi variabel
 
+    tinggi = h0
+    hasil = []
+
+    while tinggi >= h_min:
+        hasil.append(tinggi)
+        tinggi *= e
+
+    return hasil
+
+if __name__ == "__main__":
+    # PARAMETER
+    h0 = 10.0
+    e = 0.6
+    h_min = 0.01
+
+    # SIMULASI
+    hasil = pantulan_iteratif(h0, e, h_min)
+    # OUTPUT
+    print("Jumlah pantulan (iteratif):", len(hasil))
